e0ec1aeaaaec4ba595401ffa23be9e2a828e0adf
Alejandro <alejandroroasebastian9@gmail.com>
2025-04-08 13:53:41 +0200

    Subida Inicial

---
+++ src/main/java/com/calc/App.java
@@ -0,0 +1,10 @@
+package com.calc;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calculadora c1 = new Calculadora(5, 8);
+        System.out.println(c1.suma());
+    }
+}
